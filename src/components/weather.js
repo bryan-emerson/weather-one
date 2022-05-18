c504636f6ec6d151d0820fd7f6537c7997d41001
Bryan Emerson <bryan.l.emerson@gmail.com>
2022-05-18 13:20:49 -0400

    mini style

--- src/components/weather.js
+++ src/components/weather.js
@@ -1,29 +1,28 @@
 import React from 'react';
 import Card from 'react-bootstrap/Card';
-//import './styles.css';
+import './style.css';
 
 export default function Weather(props) {
   return (
     <div>
-      <div> "Location: " { props.weatherData.name } </div>
-      <div> "Visibility: " { props.weatherData.visibility } </div>
-      <div> "Current Temp: " { props.weatherData.main.temp } &deg;C</div>
-      <div> "Today's High Temp: " { props.weatherData.main.temp_max } &deg;C</div>
-      <div> "Today's Low Temp: " { props.weatherData.main.temp_min } &deg;C</div>
-      <div> "Dewpoint: " { props.weatherData.main.humidity } </div>
-      <div> "Wind: " { props.weatherData.wind["speed"] } </div>
+      <div> "Location: " {props.weatherData.name} </div>
+      <div> "Visibility: " {props.weatherData.visibility} </div>
+      <div> "Current Temp: " {props.weatherData.main.temp} &deg;C</div>
+      <div> "Today's High Temp: " {props.weatherData.main.temp_max} &deg;C</div>
+      <div> "Today's Low Temp: " {props.weatherData.main.temp_min} &deg;C</div>
+      <div> "Dewpoint: " {props.weatherData.main.humidity} </div>
+      <div> "Wind: " {props.weatherData.wind["speed"]} </div>
       <Card style={{ width: '18rem' }}>
-  <Card.Body>
-    <Card.Title>Card Title</Card.Title>
-    <Card.Subtitle className="mb-2 text-muted">Card Subtitle</Card.Subtitle>
-    <Card.Text>
-      Some quick example text to build on the card title and make up the bulk of
-      the card's content.
-    </Card.Text>
-    <Card.Link href="#">Card Link</Card.Link>
-    <Card.Link href="#">Another Link</Card.Link>
-  </Card.Body>
-</Card>
+        <Card.Body>
+          <Card.Title>{props.weatherData.name}</Card.Title>
+          <Card.Subtitle className="mb-2 text-muted">Card Subtitle</Card.Subtitle>
+          <Card.Text>
+            Some quick example text to build on the card title and make up the bulk of
+            the card's content.
+          </Card.Text>
+          <Card.Link href="#">Card Link</Card.Link>
+        </Card.Body>
+      </Card>
     </div>
   );
 }
