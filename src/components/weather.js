3051167e1a356ae36cc5c5decc0f6071d0dc2040
Bryan Emerson <bryan.l.emerson@gmail.com>
2022-05-18 16:07:03 -0400

    card popd

--- src/components/weather.js
+++ src/components/weather.js
@@ -1,24 +1,24 @@
 import React from 'react';
 import Card from 'react-bootstrap/Card';
 import './style.css';
+import moment from 'moment';
 
 export default function Weather(props) {
   return (
     <div>
-      <div> "Location: " {props.weatherData.name} </div>
-      <div> "Visibility: " {props.weatherData.visibility} </div>
-      <div> "Current Temp: " {props.weatherData.main.temp} &deg;C</div>
-      <div> "Today's High Temp: " {props.weatherData.main.temp_max} &deg;C</div>
-      <div> "Today's Low Temp: " {props.weatherData.main.temp_min} &deg;C</div>
-      <div> "Dewpoint: " {props.weatherData.main.humidity} </div>
-      <div> "Wind: " {props.weatherData.wind["speed"]} </div>
+
       <Card style={{ width: '18rem' }}>
         <Card.Body>
-          <Card.Title>{props.weatherData.name}</Card.Title>
-          <Card.Subtitle className="mb-2 text-muted">Card Subtitle</Card.Subtitle>
+          <Card.Title>{props.weatherData.name} weather</Card.Title>
+          <Card.Subtitle className="mb-2 text-muted">for {moment().format('dddd')} {moment().format('LL')}</Card.Subtitle>
           <Card.Text>
-            Some quick example text to build on the card title and make up the bulk of
-            the card's content.
+            <div> "Location: " {props.weatherData.name} </div>
+            <div> "Visibility: " {props.weatherData.visibility} </div>
+            <div> "Current Temp: " {props.weatherData.main.temp} &deg;C</div>
+            <div> "Today's High Temp: " {props.weatherData.main.temp_max} &deg;C</div>
+            <div> "Today's Low Temp: " {props.weatherData.main.temp_min} &deg;C</div>
+            <div> "Dewpoint: " {props.weatherData.main.humidity} </div>
+            <div> "Wind: " {props.weatherData.wind["speed"]} </div>
           </Card.Text>
           <Card.Link href="#">Card Link</Card.Link>
         </Card.Body>
