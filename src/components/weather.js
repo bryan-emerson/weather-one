1ce5a033c0d523c3b55b50c6b812283d20a69ec6
Bryan Emerson <bryan.l.emerson@gmail.com>
2022-05-19 10:09:17 -0400

    styling

--- src/components/weather.js
+++ src/components/weather.js
@@ -12,13 +12,12 @@ export default function Weather(props) {
           <Card.Title>{props.weatherData.name} weather</Card.Title>
           <Card.Subtitle className="mb-2 text-muted">for {moment().format('dddd')} {moment().format('LL')}</Card.Subtitle>
           <Card.Text>
-            <div> "Location: " {props.weatherData.name} </div>
-            <div> "Visibility: " {props.weatherData.visibility} </div>
-            <div> "Current Temp: " {props.weatherData.main.temp} &deg;C</div>
-            <div> "Today's High Temp: " {props.weatherData.main.temp_max} &deg;C</div>
-            <div> "Today's Low Temp: " {props.weatherData.main.temp_min} &deg;C</div>
-            <div> "Dewpoint: " {props.weatherData.main.humidity} </div>
-            <div> "Wind: " {props.weatherData.wind["speed"]} </div>
+            <div>Visibility: {props.weatherData.visibility} </div>
+            <div>Current Temp: {props.weatherData.main.temp} &deg;C</div>
+            <div>Today's High Temp: {props.weatherData.main.temp_max} &deg;C</div>
+            <div>Today's Low Temp: {props.weatherData.main.temp_min} &deg;C</div>
+            <div>Dewpoint: {props.weatherData.main.humidity} </div>
+            <div>Wind: {props.weatherData.wind["speed"]} </div>
           </Card.Text>
           <Card.Link href="#">Card Link</Card.Link>
         </Card.Body>
